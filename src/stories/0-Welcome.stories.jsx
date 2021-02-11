d4eb1a6629b69c9e1a0925f3eba610fb299e8a29
Jonatas Oliveira <jhonnyoliveira.jhon@gmail.com>
2021-02-10 21:23:36 -0300

    add welcome stories

---
+++ src/stories/0-Welcome.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const welcome = {
+  title: "Introduction/Welcome",
+};
+export default welcome;
+
+export const toAutoEscolaNardini = () => (
+  <div>
+    <h1>Auto Escola Nardini</h1>
+    <p>
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has
+      roots in a piece of classical Latin literature from 45 BC, making it over
+    </p>
+  </div>
+);
