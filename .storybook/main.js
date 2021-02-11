9bebe5d91b8f8cfdfff45f3534b96ab6627fb51e
Jonatas Oliveira <jhonnyoliveira.jhon@gmail.com>
2021-02-10 21:47:32 -0300

    fix font-familly

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
     "../src/stories/*.stories.@(js|jsx|ts|tsx|mdx)",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../src/styles/*.stories.(js|mdx)",
   ],
   addons: [
