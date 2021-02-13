263af17c60451575669e656bff02c7551184049c
Jonatas Oliveira <jhonnyoliveira.jhon@gmail.com>
2021-02-12 21:35:31 -0300

    add theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,12 @@ import React from "react";
 import { addDecorator, addParameters } from "@storybook/react";
 
 import GlobalStyle from "../src/styles/GlobalStyles";
+import ThemeProvider from "../src/styles/ThemeProvider";
 
 addDecorator((storyFn) => (
   <>
     <GlobalStyle />
-    {storyFn()}
+    <ThemeProvider>{storyFn()}</ThemeProvider>
   </>
 ));
 
