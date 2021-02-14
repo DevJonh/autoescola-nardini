dda288312289327b67c96cfdf59b48c15fbf130b
Jonatas Oliveira <jhonnyoliveira.jhon@gmail.com>
2021-02-14 10:54:46 -0300

    add Grid component

---
+++ src/components/Grid/Grid.stories.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+import Grid from ".";
+
+const grid = {
+  title: "Components/Atoms/Grid",
+  component: Grid,
+};
+
+export default grid;
+
+const Box = styled.div`
+  height: 200px;
+  background-color: indigo;
+`;
+
+export const usage = () => (
+  <Grid sm={2} md={3} lg={4} xl={5}>
+    <Box></Box>
+    <Box></Box>
+    <Box></Box>
+    <Box></Box>
+    <Box></Box>
+    <Box></Box>
+    <Box></Box>
+    <Box></Box>
+  </Grid>
+);
---
+++ src/components/Grid/index.js
@@ -0,0 +1,56 @@
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
+
+import { breakAt, BreakPoints } from "../../styles/BreakPoints";
+
+const Grid = styled.div`
+  display: grid;
+  gap: 16px;
+
+  grid-template-columns: 1fr;
+
+  ${({ sm }) =>
+    sm &&
+    css`
+      ${breakAt(BreakPoints.sm)} {
+        grid-template-columns: repeat(${({ sm }) => sm}, 1fr);
+      }
+    `}
+
+  ${({ md }) =>
+    md &&
+    css`
+      ${breakAt(BreakPoints.md)} {
+        grid-template-columns: repeat(${({ md }) => md}, 1fr);
+      }
+    `}
+  ${({ lg }) =>
+    lg &&
+    css`
+      ${breakAt(BreakPoints.lg)} {
+        grid-template-columns: repeat(${({ lg }) => lg}, 1fr);
+      }
+    `}
+  ${({ xl }) =>
+    xl &&
+    css`
+      ${breakAt(BreakPoints.xl)} {
+        grid-template-columns: repeat(${({ xl }) => xl}, 1fr);
+      }
+    `}
+`;
+
+Grid.defaultProps = {
+  sm: undefined,
+  md: undefined,
+  lg: undefined,
+  xl: undefined,
+};
+
+Grid.protoTypes = {
+  sm: PropTypes.number,
+  md: PropTypes.number,
+  lg: PropTypes.number,
+  xl: PropTypes.number,
+};
+export default Grid;
